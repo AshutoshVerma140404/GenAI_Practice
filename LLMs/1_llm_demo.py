f70c70f782abfa1f5d90e3afd34bdd65511c55c9
AshutoshVerma140404 <happy786wwe@gmail.com>
2026-08-21 00:02:40 +0530

    Update LLM demo with temperature and prompt adjustments

--- LLMs/1_llm_demo.py
+++ LLMs/1_llm_demo.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model = "gemini-3.7-flash")
+llm = ChatGoogleGenerativeAI(model = "gemini-3.7-flash", temperature = 1.5)
 
-result = llm.invoke("What is the capital of india")
+result = llm.invoke("What is the capital of india. and how are you doing today? please respond in a fun and engaging manner.")
 
 print(result.content[0]["text"])
