c67da0ee95d3e3fbb6c1a14724f41ebcf8b867fb
AshutoshVerma140404 <happy786wwe@gmail.com>
2026-09-06 22:19:46 +0530

    Add sequential LCEL chain example

---
+++ LangChain_Chains/sequential_chain.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "meta-llama/Llama-3.1-8B-Instruct",
+    task = "text-generation"
+)
+
+prompt1 = PromptTemplate(
+    template = 'Generate a detailed report on {topic}',
+    input_variables = ['topic']
+)
+
+prompt2 = PromptTemplate(
+    template = 'Generate a 5 pointer summary from the following report \n {text}',
+    input_variables = ['text']
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+final_result = chain.invoke({'topic' : 'Cricket'})
+print(final_result)
+chain.get_graph().print_ascii()
